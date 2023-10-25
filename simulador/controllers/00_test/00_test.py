2bd15efce36a696b0999782e8a5e83590e0b9b40
rir001 <riveraldrv@uc.cl>
2023-10-24 21:16:44 -0300

    codigo ayudantia

--- simulador/controllers/00_test/00_test.py
+++ simulador/controllers/00_test/00_test.py
@@ -1,3 +1,30 @@
 from controller import Robot
 
 robot = Robot()
+
+timestep = int(robot.getBasicTimeStep())
+
+motor_l  = robot.getDevice('left wheel motor')
+motor_l.setPosition(float("inf"))
+
+motor_r  = robot.getDevice('right wheel motor')
+motor_r.setPosition(float("inf"))
+
+
+lidar = robot.getDevice('LDS-01')
+lidar.enable(timestep)
+
+
+
+print(dir(lidar))
+
+
+while robot.step(timestep) != -1:
+
+    motor_l.setVelocity(3)
+    motor_r.setVelocity(3)
+
+    # print(lidar.getRangeImage())
+
+
+    pass
