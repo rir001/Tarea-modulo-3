0d6642c65b8efedc0af7d37962c9e019e2804403
rir001 <riveraldrv@uc.cl>
2023-11-09 21:59:38 -0300

    Actualizacion de enunciado

--- simulador/controllers/03_location_obstacle/03_location_obstacle.py
+++ simulador/controllers/03_location_obstacle/03_location_obstacle.py
@@ -5,9 +5,7 @@ robot = Robot()
 timestep = int(robot.getBasicTimeStep())
 
 robot.set_GPS()
-robot.set_Gyro()
 
 
 while robot.step(timestep) != -1:
     print(robot.gpsGetValues())
-    print(robot.gyroGetValues())
--- simulador/controllers/03_location_obstacle/local_robot.py
+++ simulador/controllers/03_location_obstacle/local_robot.py
@@ -12,11 +12,3 @@ class Robot(base_robot):
 
     def gpsGetValues(self):
         return np.array(self.gps.getValues()) + self._desfase_gps
-
-    def set_Gyro(self):
-        self.gyro = self.getDevice('gyro')
-        self.gyro.enable(int(self.getBasicTimeStep()))
-        self._desfase_gyro = [np.random.randint(-1, 1)]*2 + [0]
-
-    def gyroGetValues(self):
-        return np.array(self.gyro.getValues()) + self._desfase_gyro
