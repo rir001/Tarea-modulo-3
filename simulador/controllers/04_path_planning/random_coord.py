140fdb327a31112c93afee1881b204b7fcbf95a2
rir001 <riveraldrv@uc.cl>
2023-11-23 01:38:50 -0300

    fix 2 random coord

--- simulador/controllers/04_path_planning/random_coord.py
+++ simulador/controllers/04_path_planning/random_coord.py
@@ -1,15 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def get_points(n=2):
     """Genera n coordenadas aleatorias en el mapa"""
     _mapa = plt.imread("maze.png")[::, ::, 0]
     paths = []
     while len(paths) < n:
         x, y = np.random.randint(0, len(_mapa), 2)
-        if _mapa[y, x] == 1 and \
-            [x, y] not in [[int(n[0]), int(n[1])] for n in paths] and \
-            all([7<(int(x-n[0])**2 + int(y-n[1])**2) for n in paths]):
-            paths.append([x+np.random.rand()*0.6+0.2, y+np.random.rand()*0.6+0.2])
+        if (_mapa[y, x] == 1) and [y, x] not in paths:
+            if all([np.linalg.norm(np.array([y, x]) - np.array(p)) > 10 for p in paths]):
+                paths.append([y, x])
 
-    return (np.array(paths) - len(_mapa) / 2) / 5
+    for n in range(len(paths)): paths[n] += 0.4 + np.random.rand(2) * 0.2
+
+    return (np.array(paths) - (len(_mapa) / 2)) / 5
